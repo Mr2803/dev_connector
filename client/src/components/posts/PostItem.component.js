bd6454dc17c10a48d6231ce0af94c2a588b64080
Mr2803 <rispoli.mar2803@gmail.com>
2021-07-08 15:57:14 +0200

    display post on a screen , only logged user

---
+++ client/src/components/posts/PostItem.component.js
@@ -0,0 +1,57 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Moment from "react-moment";
+import { connect } from "react-redux";
+
+const PostItem = ({
+  post: { _id, text, name, avatar, user, likes, comments, date },
+  auth,
+}) => {
+  return (
+    <div className="post bg-white p-1 my-1">
+      <div>
+        <Link to={`/profile/${user}`}>
+          <img className="round-img" src={avatar} alt="Profile_Image" />
+          <h4>{name}</h4>
+        </Link>
+      </div>
+      <div>
+        <p className="my-1">{text}</p>
+        <p className="post-date">
+          Postato il <Moment format="DD/MM/YYYY HH:mm">{date}</Moment>
+        </p>
+        <button type="button" className="btn btn-light">
+          <i className="fas fa-thumbs-up"></i>
+          {likes.length > 0 && <span>{likes.length}</span>}
+        </button>
+        <button type="button" className="btn btn-light">
+          <i className="fas fa-thumbs-down"></i>
+        </button>
+        <Link to={`/post/${_id}`} className="btn btn-primary">
+          Commenti
+          {comments.length > 0 && (
+            <span className="comment-count">{comments.length}</span>
+          )}
+        </Link>
+        {!auth.loading && user === auth.user._id}
+        {
+          <button type="button" className="btn btn-danger">
+            <i className="fas fa-times"></i>
+          </button>
+        }
+      </div>
+    </div>
+  );
+};
+
+PostItem.propTypes = {
+  post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, {})(PostItem);
--- client/src/components/posts/Posts.component.js
+++ client/src/components/posts/Posts.component.js
@@ -2,13 +2,30 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner.component";
+import PostItem from "./PostItem.component";
 import { getPosts } from "../../actions/post.action";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div></div>;
+  console.log(posts);
+  return loading ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Benvenuto nella comunity
+      </p>
+      {/* Post form */}
+      <div className="posts">
+        {posts.map((post) => {
+          return <PostItem key={post._id} post={post} />;
+        })}
+      </div>
+    </>
+  );
 };
 
 Posts.propTypes = {

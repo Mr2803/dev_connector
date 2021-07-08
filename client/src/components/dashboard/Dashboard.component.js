1baf588b99c38b62d9dfa409aba9d11b2781ac15
Mr2803 <rispoli.mar2803@gmail.com>
2021-07-08 21:24:32 +0200

    minors changes

--- client/src/components/dashboard/Dashboard.component.js
+++ client/src/components/dashboard/Dashboard.component.js
@@ -30,7 +30,7 @@ const Dashboard = ({
       </p>
       {profile !== null ? (
         <>
-          <DashboardActions />
+          <DashboardActions user={profile.user} />
           <Experience experience={profile.experience} />
           <Education education={profile.education} />
           <div className="my-2">
--- client/src/components/dashboard/DashboardActions.component.js
+++ client/src/components/dashboard/DashboardActions.component.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const DashboardActions = () => {
+const DashboardActions = ({ user: { _id } }) => {
+  console.log(_id);
   return (
     <div className="dash-buttons">
       <Link to="/edit-profile" className="btn btn-light">
@@ -12,6 +13,9 @@ const DashboardActions = () => {
       </Link>
       <Link to="/add-education" className="btn btn-light">
         <i className="fas fa-graduation-cap text-primary"></i> Add Education
+      </Link>
+      <Link to={`/profile/${_id}`} className="btn btn-light">
+        <i className="fas fa-users text-primary"></i> Profilo Pubblico
       </Link>
     </div>
   );

c40d8cffa010885e9ebe9986502da7df35e57484
Mr2803 <rispoli.mar2803@gmail.com>
2021-07-09 13:58:53 +0200

    minor-change in navbar

--- client/src/components/layout/Navbar.component.js
+++ client/src/components/layout/Navbar.component.js
@@ -20,10 +20,10 @@ function Navbar({ auth: { isAuth, loading }, logout }) {
         </Link>
       </li>
       <li>
-        <a onClick={logout} href="#!">
+        <Link onClick={logout} to="/login">
           <i className="fas fa-sign-out-alt"></i>
           <span className="hide-sm">Logout</span>
-        </a>
+        </Link>
       </li>
     </ul>
   );

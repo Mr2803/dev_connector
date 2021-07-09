ced36044f32376413fd4e6f529debb66193166e2
Mr2803 <rispoli.mar2803@gmail.com>
2021-07-09 15:18:28 +0200

    prepare to deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { connect } = require("mongoose");
 const connectDB = require("./config/db");
-const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -11,7 +11,7 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Api Running"));
+// app.get("/", (req, res) => res.send("Api Running"));
 
 //Define Routes
 
@@ -19,6 +19,16 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
